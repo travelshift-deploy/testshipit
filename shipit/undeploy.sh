8df9ff1ff53df0f57d695634de24bb9dd60d082e
GTI Shipit <shipit@guidetoiceland.is>
2017-11-14 05:40:27 +0000

    Adding rollback

---
+++ shipit/undeploy.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+STAMP=$(date +"%y%m%d %H:%M")
+DBMIGRATE=0
+WEBCLIENT=0
+CACHEWIPE=0
+echo "$STAMP: Reverting deploy"
+echo "-----------------------------------------"
+echo "TARGET=$REVISION"
+echo "-----------------------------------------"
+
+commits=$(git log --reverse --pretty=format:"%h" $REVISION...HEAD | tr '\n' ' ')
+echo "Commits to reverse : $commits"
+echo "Checking if optional steps needed"
+echo -n " - db migrations ... "
+for commit in $commits
+do
+	if [[ "$(git diff-tree --no-commit-id --name-only -r $commit | grep -E '^db/' | wc -l)" != "0" ]]
+	then
+	        DBMIGRATE=1
+	        break
+	fi
+done
+if [ "$DBMIGRATE" == "1" ]
+then
+	echo "will need to migrate"
+else
+	echo "not needed"
+fi
+
+
+echo -n " - cache wipeout ... "
+for commit in $commits
+do
+        if [[ "$(git diff-tree --no-commit-id --name-only -r $commit | grep -E '(css|less|sass|js)' | wc -l)" != "0" ]]
+        then
+                DBMIGRATE=1
+                break
+        fi
+done
+
+
+if [ "$CACHEWIPE" == "1" ]
+then
+        echo "will need to clean"
+else
+        echo "not needed"
+fi
+
+echo -n " - webclient rebuild ... "
+for commit in $commits
+do
+        if [[ "$(git diff-tree --no-commit-id --name-only -r $commit | grep -E 'webclient/' | wc -l)" != "0" ]]
+        then
+                WEBCLIENT=1
+                break
+        fi
+done
+
+
+if [ "$WEBCLIENT" == "1" ]
+then
+        echo "will need to rebuild"
+else
+        echo "not needed"
+fi
+
