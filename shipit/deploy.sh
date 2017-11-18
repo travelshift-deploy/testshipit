77294782ec557ecff3ccec97bb3416cd293c7bce
GTI Shipit <shipit@guidetoiceland.is>
2017-11-18 02:31:11 +0000

    typo fix

--- shipit/deploy.sh
+++ shipit/deploy.sh
@@ -17,7 +17,7 @@ echo "Checking if optional steps needed"
 echo -n " - db migrations ... "
 for commit in $commits
 do
-	if [[ "$(git diff-tree --no-commit-id --name-only -r $commit | grep -e '^db/' -e '^migrations/| wc -l)" != "0" ]]
+	if [[ "$(git diff-tree --no-commit-id --name-only -r $commit | grep -e '^db/' -e '^migrations/'| wc -l)" != "0" ]]
 	then
 	        DBMIGRATE=1
 	        break
